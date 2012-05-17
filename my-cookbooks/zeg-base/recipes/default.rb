c454089d35b4153f24691434b3887b56cacdf90d
Klaus Alfert <klaus.alfert@googlemail.com>
2012-05-17 22:25:50 +0200

    Weitere Rezepte hinzugefügt.

---
+++ my-cookbooks/zeg-base/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: zeg-base
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# mein Lieblingseditor
+package "joe" do
+	action :install
+end
