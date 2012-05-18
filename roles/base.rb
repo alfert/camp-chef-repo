6f01a33742be7980c3ebea289db3ae26af85b89e
Klaus Alfert <klaus.alfert@googlemail.com>
2012-05-18 17:32:42 +0200

    Das Default-Environment heißt "_default"

--- roles/base.rb
+++ roles/base.rb
@@ -3,11 +3,15 @@ description "base role applied to all nodes with some default settings."
     
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 env_run_list({
-	:default => ["apt", "apt::cacher-client", "zeg-base"]
+	"_default" => ["apt", "apt::cacher-client", "zeg-base"]
 })
     
 # Attributes applied if the node doesn't have it set already.
-default_attributes()
+default_attributes({
+	:apt => {
+		:cacher_ipaddress => "192.168.2.102"
+	}
+	})
 
 # Attributes applied no matter what the node has set already.
 override_attributes()
