7eb6ac3aae4616f6e2c74aa4ee726ed10fe9b322
Klaus Alfert <klaus.alfert@googlemail.com>
2012-05-18 15:56:40 +0200

    Basis-Recipe und Basis-Rolle erweitert

--- my-cookbooks/zeg-base/recipes/default.rb
+++ my-cookbooks/zeg-base/recipes/default.rb
@@ -2,12 +2,23 @@
 # Cookbook Name:: zeg-base
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Zühlke Engineering GmbH
 #
 # All rights reserved - Do Not Redistribute
 #
 
+# es sollte schon deutsch sein ...
+package "language-pack-de" do
+	action :install
+end
+
+
 # mein Lieblingseditor
 package "joe" do
+	action :install
+end
+
+# unzip braucht man auch immer ...
+package "unzip" do
 	action :install
 end
--- roles/base.rb
+++ roles/base.rb
@@ -2,9 +2,9 @@ name "base"
 description "base role applied to all nodes with some default settings."
     
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-env_run_list{
-	:default => "zeg-base"
-}
+env_run_list({
+	:default => ["apt", "apt::cacher-client", "zeg-base"]
+})
     
 # Attributes applied if the node doesn't have it set already.
 default_attributes()
