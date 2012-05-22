3c581cf8e970d02a6fe9dc0f88efb9e205380da2
Klaus Alfert <klaus.alfert@googlemail.com>
2012-05-22 14:13:29 +0200

    Das Rezept heißt anders

--- roles/apt-cacher.rb
+++ roles/apt-cacher.rb
@@ -3,8 +3,8 @@ description "base role applied to all nodes with some default settings."
     
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 env_run_list({
-	"_default" => ["role[base]", "apt-cacher-ng"],
-	"camp2012" => ["role[base]", "apt-cacher-ng"]
+	"_default" => ["role[base]", "apt::apt-cacher-ng"],
+	"camp2012" => ["role[base]", "apt::apt-cacher-ng"]
 })
     
 # Attributes applied if the node doesn't have it set already.
