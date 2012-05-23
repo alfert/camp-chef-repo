90d3a783ef271007434c6431cd9d7d06e2b6c3aa
Klaus Alfert <klaus.alfert@googlemail.com>
2012-05-23 12:22:03 +0200

    Mit ChefServer wird pgpool verwendet

--- roles/db-server.rb
+++ roles/db-server.rb
@@ -4,11 +4,18 @@ description "postgresql server role."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 env_run_list({
 	"_default" => ["role[base]", "postgresql::server", "database", "camp-pg-demo-chef"],
-	"camp2012" => ["role[base]", "postgresql::server", "database", "camp-pg-demo-chef"]
+	"camp2012" => ["role[base]", "postgresql::server", "pgpool2", "database", "camp-pg-demo-chef"]
 })
     
 # Attributes applied if the node doesn't have it set already.
-default_attributes()
+default_attributes({
+	'postgresql' => {
+		'port' => '15432'
+	},
+	'pgpool2' => {
+		'port' => '5432'
+	}
+	})
 
 # Attributes applied no matter what the node has set already.
 override_attributes()
